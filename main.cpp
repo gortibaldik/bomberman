7e02e65df48c42b540d4ae869b1ffeef98ac4988
gortibaldik <ferotre@gmail.com>
2021-09-14 18:58:14 +0200

    Add cmd arg for getting media directory path

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "window_manager/def.hpp"
 #include "states/start.hpp"
+#include <iostream>
 
 int main(int argc, char** argv) {
-    WindowManager wm;
-    wm.push_state(std::make_unique<StartState>(wm));
-    wm.loop();
+  if (argc != 2) {
+    std::cout << "Invalid arg count" << std::endl;
+    return 2;
+  }
+  WindowManager wm(argv[1]);
+  wm.push_state(std::make_unique<StartState>(wm));
+  wm.loop();
 }
