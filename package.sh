f1368c2824bbea93a13831e7f0b76b6ece5ae452
gortibaldik <ferotre@gmail.com>
2021-09-14 19:35:56 +0200

    Add post-instalation and pre-remove scripts to package

--- package.sh
+++ package.sh
@@ -20,6 +20,25 @@ Description: Implementation of bomberman, which is playable over the local netwo
 Depends: libc6 (>= 2.30), libfreetype6 (>= 2.2.1), libgcc-s1 (>= 3.0), libstdc++6 (>= 9), libudev1 (>= 183), libx11-6, libxcursor1 (>> 1.1.2), libxrandr2 (>= 2:1.2.99.3)
 """ > ${package_name}/DEBIAN/control
 
+desktop_launcher="/usr/share/applications/Bomberman.desktop"
+echo """#!/bin/sh
+sudo echo \"\"\"[Desktop Entry]
+Type=Application
+Exec=/usr/local/bin/Bomberman
+Name=Bomberman 0.1
+GenericName=Bomberman
+Categories=Game;
+\"\"\" > ${desktop_launcher}
+sudo update-desktop-database
+""" > ${package_name}/DEBIAN/postinst
+
+echo """#!/bin/sh
+sudo rm ${desktop_launcher}
+""" > ${package_name}/DEBIAN/prerm
+
+chmod +x ${package_name}/DEBIAN/postinst
+chmod +x ${package_name}/DEBIAN/prerm
+
 # build the deb package
 dpkg-deb --build --root-owner-group ${package_name}
 rm -r $package_name
